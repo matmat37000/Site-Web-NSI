16c2e430abdc714244e84d7bb5ba55bb5b093d1f
Mathieu Bordier-Aupy <mathieu.bordieraupy@gmail.com>
2024-02-20 19:15:57 +0100

    Update main page and javascript
    Co-authored-by: MathChameleon <MathChameleon@users.noreply.github.com>

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,22 @@
+
+let root = document.documentElement;
+var isDay = true;
+
 function funcDayToNight() {
-  document.querySelector("body") .classList.remove("Day");
-  document.querySelector("body") .classList.remove("Night")
+  if (isDay) { 
+    // Night Mode
+    root.style.setProperty('--black-font-color', "#e0eaeb");
+    root.style.setProperty('--white-font-color', "#1f1f1f");
+    root.style.setProperty('--background-color', "#142755");
+    root.style.setProperty('--secondary-background-color', "#071141");
+    isDay = false;
+  }
+  else {
+    // Day Mode
+    root.style.setProperty('--black-font-color', "#1f1f1f");
+    root.style.setProperty('--white-font-color', "#e0eaeb");
+    root.style.setProperty('--background-color', "#ebd7aa");
+    root.style.setProperty('--secondary-background-color', "#e6bd60");
+    isDay = true;
+  }
 };
