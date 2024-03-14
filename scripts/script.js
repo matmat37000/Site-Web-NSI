3cc2b91e1cd46d1d42d2fbf7bdd6e017925b15fc
Mathieu Bordier-Aupy <mathieu.bordieraupy@gmail.com>
2024-03-14 09:06:40 +0100

    Update flower

--- scripts/script.js
+++ scripts/script.js
@@ -6,15 +6,16 @@ var isDay = true;
 function funcDayToNight() {
   if (isDay) { 
     console.log(isDay);
-  
     // Day Mode
     // document.querySelector.add('class="fa-solid fa-sun"');
     // document.querySelector.remove('class="fa-solid fa-moon"');
-    root.style.setProperty('--black-font-color', "#1f1f1f");
-    root.style.setProperty('--white-font-color', "#e0eaeb");
-    root.style.setProperty('--background-color', "#ebd7aa");
-    root.style.setProperty('--secondary-background-color', "#e6bd60");
-    root.style.setProperty('--day', "#ffc400");
+    root.style.setProperty('--black-font-color', "#02111B");
+    root.style.setProperty('--white-font-color', "#fbffff");
+    root.style.setProperty('--background-color', "#c1e857");
+    root.style.setProperty('--secondary-background-color', "#325327");
+    root.style.setProperty('--day', "#325327");
+    var image = document.getElementById("button-icon-cb");
+		image.src = "../svg/sun.svg";
     isDay = false;
   }
   else {
@@ -22,11 +23,13 @@ function funcDayToNight() {
     // Night Mode
     // button.remove('class="fa-solid fa-sun"');
     // button.add('class="fa-solid fa-moon"');
-    root.style.setProperty('--black-font-color', "#e0eaeb");
-    root.style.setProperty('--white-font-color', "#1f1f1f");
-    root.style.setProperty('--background-color', "#142755");
-    root.style.setProperty('--secondary-background-color', "#071141");
-    root.style.setProperty('--day', "#000e4d");
+    root.style.setProperty('--black-font-color', "#fbffff");
+    root.style.setProperty('--white-font-color', "#02111B");
+    root.style.setProperty('--background-color', "#325327");
+    root.style.setProperty('--secondary-background-color', "#c1e857");
+    root.style.setProperty('--day', "#c1e857");
+    var image = document.getElementById("button-icon-cb");
+		image.src = "../svg/moon.svg";
     isDay = true;
   }
 };
